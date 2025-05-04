94427fb1b7ef12ad1d7ddc2645db0adc80de9379
Allan Barillas <93857150+ABSosa028@users.noreply.github.com>
2025-05-04 11:08:12 -0600

    toco subir por partes el commit por eliminacion de archivos pesados

--- app.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-new_model = tf.keras.models.load_model('modelos/numeros.h5')
-
-#carga imagen de mi carpeta
-img = tf.keras.preprocessing.image.load_img('datasets/numeros/1/1_1746303283982.png', target_size=(28, 28), color_mode='grayscale')
-
-#print(new_model.predict('datasets/numeros/1/1_1746303283982.png').shape)
